668d9822da243a4eb18dc8a823531f1030263c3b
johyeon yoon <yds03080@naver.com>
2021-09-13 10:32:00 +0900

    Initial commit

---
+++ MiniGame/MiniGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniGame
+//
+//  Created by Johyeon Yoon on 2021/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniGame/MiniGame/MiniGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniGameApp.swift
+//  MiniGame
+//
+//  Created by Johyeon Yoon on 2021/09/13.
+//
+
+import SwiftUI
+
+@main
+struct MiniGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
